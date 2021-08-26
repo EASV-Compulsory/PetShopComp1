3168b8d61397e15850cf75e9acff785140925512
kubaczechowski <jcobczechowski@gmail.com>
2021-08-26 11:48:49 +0200

    added Utils.cs and IUtils.cs

---
+++ PetShop.UI/IUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using PetShop.Core.Models;
+
+namespace PetShop.UI
+{
+    public interface IUtils
+    {
+        //for getting option 
+        void ReadIntegerFromString(out int selectedNumber, string messageToUser);
+        //Color & Name
+        void GetMinimalStringInput(out string userInput, int minLength, string repeatMessage);
+        // birthdate & selldate
+        void GetDateInput(out DateTime date);
+        //Price
+        void ReadDoubleFromString(out double price, string messageToUser);
+        //Options
+        string ConcatPossibleOptions();
+        
+
+    }
+}
---
+++ PetShop.UI/Utils.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using PetShop.Core.Models;
+
+namespace PetShop.UI
+{
+    public class Utils :IUtils
+    {
+        public void ReadIntegerFromString(out int selectedNumber, string messageToUser)
+        {
+            var consoleInput = Console.ReadLine();
+            int intValue; 
+            while (!int.TryParse(consoleInput, out intValue))
+            {
+                Console.WriteLine(messageToUser);
+                consoleInput = Console.ReadLine();
+            }
+            selectedNumber = intValue;
+        }
+
+        public void GetMinimalStringInput(out string userInput, int minLength, string repeatMessage)
+        {
+            var inp = Console.ReadLine();
+            while (inp.Length < minLength)
+            {
+                Console.WriteLine(repeatMessage);
+                inp = Console.ReadLine();
+            }
+            userInput = inp;
+        }
+
+        public void GetDateInput(out DateTime relaseDate)
+        {
+            var dateTime = Console.ReadLine();
+            DateTime result;
+            while (!DateTime.TryParse(dateTime, out result))
+            {
+                Console.WriteLine(StringConstants.IncorrectDateFormat);
+                dateTime = Console.ReadLine();
+            }
+            relaseDate = result;
+        }
+
+        public void ReadDoubleFromString(out double price, string messageToUser)
+        {
+            var consoleInput = Console.ReadLine();
+            double doubleValue; 
+            while (!double.TryParse(consoleInput, out doubleValue))
+            {
+                Console.WriteLine(messageToUser);
+                consoleInput = Console.ReadLine();
+            }
+            price = doubleValue;
+        }
+
+        public string ConcatPossibleOptions()
+        {
+            var output = "";
+            var enums = Enum.GetValues(typeof(Options));
+            foreach (var eEnum in enums)
+            {
+                var line = $"{(int) eEnum}. {eEnum} \n";
+                output = string.Concat(output, line);
+            }
+            return output;
+        }
+
+       
+    }
+}
