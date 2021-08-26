0a4852899c6a60260b119150fabb241e9a65693a
kubaczechowski <jcobczechowski@gmail.com>
2021-08-26 11:49:29 +0200

    added enum with Options for menu

---
+++ PetShop.UI/Options.cs
@@ -0,0 +1,14 @@
+﻿namespace PetShop.UI
+{
+    public enum Options
+    {
+        ShowAllPets =1,
+        SearchPetsByType =2,
+        CreatePet = 3,
+        DeletePet = 4,
+        UpdatePet = 5,
+        SortPetsByPrice = 6,
+        GetFiveCheapestPets =7,
+        Exit = 8
+    }
+}
